6c29026afbbf6898f9421ff2f059e5df52fe48d8
Jesus Sanchez <oeckeint@gmail.com>
2024-05-03 00:38:49 -0600

    Implementación de Pipes

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,10 +1,19 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {ProductModel} from "@shared/components/models/product.model";
+import {CurrencyPipe, DatePipe, UpperCasePipe} from "@angular/common";
+import {ReversePipe} from "@shared/pipes/reverse.pipe";
+import {TimeAgoPipe} from "@shared/pipes/time-ago.pipe";
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [
+    UpperCasePipe,
+    CurrencyPipe,
+    DatePipe,
+    ReversePipe,
+    TimeAgoPipe
+  ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
---
+++ src/app/domains/shared/pipes/time-ago.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { formatDistance } from 'date-fns';
+
+@Pipe({
+  name: 'timeAgo',
+  standalone: true
+})
+export class TimeAgoPipe implements PipeTransform {
+
+  transform(value: string): string {
+    return formatDistance(new Date(value), new Date()) + ' ago';
+  }
+
+}
